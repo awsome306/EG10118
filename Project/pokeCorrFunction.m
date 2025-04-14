d45fbd718ecbf784d94639bb4f24cdaabfde55f9
Joey Noether <71984403+awsome306@users.noreply.github.com>
2025-04-14 11:30:44 -0400

    Created pokeCorrFunction.m for plotting purposes

---
+++ Project/pokeCorrFunction.m
@@ -0,0 +1,41 @@
+function pokeCorrFunction(app)
+% POKECORRFUNCTION Generates a plot of the correlation between two
+% statistics for a given type of Pokemon.
+% If a Pokemon is weak in one stat, will it be strong, weak, or neutral in
+% another? How does this correlation changed based on the Pokemon's "type"?
+%
+% Visual: A regular plot, (with a trendline and confidence interval)
+% User Interactions:
+% * Select X and Y stats
+% * Filter by Type, or select all Pokemon
+
+% % Removed a for loop that iterated through ALL the types 
+% for i = 2:length(pokeTypes)
+%     pokeType = pokeTypes(i);
+% end
+
+pokeType= app.PokeTypeDropDown;
+statX   = app.XStatDropDow.Value;
+statY   = app.YStatDropDown.Value;
+
+pokeX   = createPokeData(T,pokeType,statX);
+pokeY   = createPokeData(T,pokeType,statY);
+
+
+% Plot to UI AXES:
+cAxes = app.UIAxes; % Set of app axes to plot on
+plot(cAxes,pokeX,pokeY,'x','DisplayName',pokeType,'Color',pokeColorDict(pokeType))
+xlabel(cAxes,statX)
+ylabel(cAxes,statY)
+legend(cAxes)
+grid(cAxes,"on")
+hold(cAxes,"on")
+
+% Create and plot trendline
+fitOrder = 1;
+p = polyfit(pokeX,pokeY,fitOrder);
+xTrendVals = min(pokeX):1:max(pokeX);
+yTrendVals = polyval(p,xTrendVals);
+
+plot(cAxes,xTrendVals,yTrendVals,'--','LineWidth',2,'DisplayName','TrendLine','Color',pokeColorDict(pokeType))
+
